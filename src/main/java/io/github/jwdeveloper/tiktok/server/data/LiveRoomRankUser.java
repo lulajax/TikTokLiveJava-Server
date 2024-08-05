51eadc9821ee8fd2f9e152d46bbcdf35fc80132a
lujunjie <junjie.lu@scienjoy.com>
2024-08-05 16:36:47 +0800

    Revert "LiveRoomRankUser 增加索引"
    This reverts commit 8217a53717f7c7c81f4cbc0256ae386497082970.

--- src/main/java/io/github/jwdeveloper/tiktok/server/data/LiveRoomRankUser.java
+++ src/main/java/io/github/jwdeveloper/tiktok/server/data/LiveRoomRankUser.java
@@ -30,8 +30,7 @@ import lombok.Data;
 @Entity
 @Table(name = "live_room_rank_user", indexes = {
         @Index(name = "idx_live_room_rank_user_roomId", columnList = "roomId"),
-        @Index(name = "idx_live_room_rank_user_hostId", columnList = "hostId"),
-        @Index(name = "idx_live_room_rank_user_roomId_hostId", columnList = "roomId,hostId")
+        @Index(name = "idx_live_room_rank_user_hostId", columnList = "hostId")
 })
 public class LiveRoomRankUser {
     @Id
